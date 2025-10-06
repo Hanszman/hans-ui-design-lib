dfa22842a579af3d24647a6a79b41b5009bb9822
Hanszman <victor.hanszman@hotmail.com>
2025-10-06 17:24:37 -0300

    fixing dev page and creating color variables

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center">
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center !my-4">
           <img
             src={vhLogo}
             className="w-6 h-auto !mx-2 !my-1"
             alt="Victor Hanszman's Logo"
           />
-          <h1 className="text-[#8257E5] !text-xl !font-bold">
+          <h1 className="text-[var(--purple-primary)] !text-xl !font-bold">
             Hanszman's UI Design Lib
           </h1>
         </div>
@@ -19,9 +19,11 @@ function App() {
             In this Lib you can develop your components for your own projects.
           </p>
           <p>
-            Execute the script &nbsp;
-            <code className="text-[#8257E5]">npm run storybook</code>&nbsp; to
-            check out the documentation
+            Check out the documentation by running the script: &nbsp;
+            <code className="text-[var(--purple-primary)]">
+              npm run storybook
+            </code>
+            .
           </p>
         </div>
       </div>
