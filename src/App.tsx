7bd5825b5b94f13ef9e2fd7908b4c62f2ead942c
Hanszman <victor.hanszman@hotmail.com>
2025-10-06 12:49:16 -0300

    starting to build de dev page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import vhLogo from './assets/img/logo/vh_logo_purple.png';
-import './styles/logo.scss';
 
 function App() {
   return (
     <>
-      <div className="flex bg-red-200 p-4">
-        <img src={vhLogo} className="logo" alt="Victor Hanszman Logo" />
-        <h1>Hanszman's UI Design Lib</h1>
+      <div className="flex flex-col items-center">
+        <div className="flex justify-center items-center">
+          <img
+            src={vhLogo}
+            className="w-6 h-auto !mx-2 !my-1"
+            alt="Victor Hanszman's Logo"
+          />
+          <h1 className="text-[#8257E5] !text-xl !font-bold">
+            Hanszman's UI Design Lib
+          </h1>
+        </div>
+        <div>
+          <p>
+            In this Lib you can develop your components for your own projects.
+          </p>
+          <p>
+            Execute the script &nbsp;
+            <code className="text-[#8257E5]">npm run storybook</code>&nbsp; to
+            check out the documentation
+          </p>
+        </div>
       </div>
     </>
   );
