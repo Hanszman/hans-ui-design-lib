370fbde17db4b31560cb75f3a5c96a6c0d2968f1
Hanszman <victor.hanszman@hotmail.com>
2025-10-23 21:45:22 -0300

    fixing props

--- src/index-wc.ts
+++ src/index-wc.ts
@@ -4,10 +4,20 @@ import * as ReactDOM from 'react-dom/client';
 import reactToWebComponent from 'react-to-webcomponent';
 import { Button } from './components/Forms/Button/Button';
 
-const HansButton = reactToWebComponent(
-  Button,
-  React,
-  ReactDOM as unknown as Parameters<typeof reactToWebComponent>[2],
-);
+const ReactDOMtoWC = ReactDOM as unknown as Parameters<
+  typeof reactToWebComponent
+>[2];
+
+const HansButton = reactToWebComponent(Button, React, ReactDOMtoWC, {
+  props: [
+    'label',
+    'size',
+    'color',
+    'variant',
+    'buttonType',
+    'customClasses',
+    'disabled',
+  ],
+});
 
 customElements.define('hans-button', HansButton);
